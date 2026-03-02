c3dbce7c89db5d2160f602247734bdea7c52d201
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-02 11:41:27 +0800

    移除 Program 类，直接输出出生地信息

--- st/st01/Program.cs
+++ st/st01/Program.cs
@@ -1,8 +1,3 @@
 ﻿
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, .NET!");
-    }
-}
+string hometown = "福州";
+Console.WriteLine($"I was born in {hometown}, which is a beautiful city in China.");
