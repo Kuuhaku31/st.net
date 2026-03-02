44f1679d2b6fdfe6e86e1532a909e7416df50880
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-02 18:35:55 +0800

    添加 ST02 项目及相关配置，包含订单类、计算器功能和主程序逻辑

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 dotnet build
 dotnet run
 ```
+
+添加到.sln文件中：
+
+```bash
+dotnet sln add <project_path>
+```
---
+++ st/st02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = new Order(1, "Alan Kay", new DateTime(2022, 2, 2));
+            Order order2 = new Order(2, "Ted Nelson", new DateTime(2022, 3, 7));
+
+            order1.DisplayOrder();
+            order2.DisplayOrder();
+        }
+    }
+}
