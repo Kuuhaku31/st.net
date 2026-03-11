4afcfadfd927411c9caaa9e9880cb7fd74221877
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-11 11:23:38 +0800

    添加计算器项目，包含主窗口、视图模型和模型，支持基本运算功能；更新项目文件以引入 CommunityToolkit.Mvvm 包

---
+++ st/st08/Operator.cs
@@ -0,0 +1,44 @@
+using System.Globalization;
+using System.Windows.Data;
+namespace Calc
+{
+    public enum Operator
+    {
+
+        Plus,
+        Minus,
+        Multiply,
+        Equal,
+        Empty
+    }
+    public class OperatorToSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            switch (value)
+            {
+                case Operator.Plus:
+                    return "+";
+                case Operator.Minus:
+                    return "-";
+                case Operator.Multiply:
+                    return "x";
+                case Operator.Equal:
+                    return "=";
+                case Operator.Empty:
+                    return "";
+                default:
+                    return "";
+
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 今回は逆変換は不要です。
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ st/st08/ViewModel.cs
@@ -0,0 +1,56 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+namespace Calc
+{
+    internal partial class ViewModel : ObservableObject
+    {
+        private Model _model;
+
+        [ObservableProperty]
+        private Operator _nextOperator = Operator.Empty;
+
+        [ObservableProperty]
+        private int _result = 0;
+
+        [ObservableProperty]
+        private int _currentInput = 0;
+
+        [RelayCommand]
+        private void ClearNumber()
+        {
+            CurrentInput = 0;
+        }
+
+        [RelayCommand]
+        private void Clear()
+        {
+            _model.Clear();
+            NextOperator = Operator.Empty;
+            Result = 0;
+            CurrentInput = 0;
+
+        }
+
+        [RelayCommand]
+        private void EnterNumber(string numStr)
+        {
+            CurrentInput = CurrentInput * 10 + Convert.ToInt32(numStr);
+        }
+
+        [RelayCommand]
+        private void EnterOperator(Operator nextOp)
+        {
+
+            Result = _model.Calc(NextOperator, CurrentInput);
+            NextOperator = nextOp;
+            CurrentInput = 0;
+
+        }
+
+        public ViewModel()
+        {
+            _model = new Model();
+        }
+
+    }
+}
