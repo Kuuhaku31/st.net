1f3b4180be51459d5e6b2b2bf624255cabc365e6
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-13 16:27:12 +0800

    新增 ST11Test 项目及其测试文件，删除 LINQ 相关文件

---
+++ st/st11/ST11Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ST11Test;
+
+using Xunit.Abstractions; // 引入 xUnit 的 ITestOutputHelper 接口以便在测试中输出日志信息
+
+public class
+UnitTest1(ITestOutputHelper output) // 通过构造函数注入 ITestOutputHelper 实例以便在测试方法中使用
+{
+    private readonly ITestOutputHelper _output = output;
+
+    [Fact]
+    public void Test1()
+    {
+        _output.WriteLine("Hello World!");
+    }
+}
