02ac137a0bb315efea4ac2f5d080b60b5d8b5f92
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-15 14:05:53 +0800

    重构 ProgramJson 类，修改书籍排序逻辑为按作者降序，并新增统计指定作者书籍数量的方法

--- st/st11/LINQ/ProgramJson.cs
+++ st/st11/LINQ/ProgramJson.cs
@@ -24,7 +24,7 @@ class ProgramJson
             ]
         }";
 
-        JObject json = JObject.Parse(jsonText);
+        var json = JObject.Parse(jsonText);
 
         // var results = json["books"]
         //     .OrderByDescending(book => book["date"])
@@ -35,17 +35,39 @@ class ProgramJson
         if(json["books"] is JArray books)
         {
             // 对数组进行排序和选择
-            var results = books
-                .OrderByDescending(book => book["date"])             // 按照日期降序排序
-                .Select(book => book["title"] + ", " + book["date"]) // 选择需要的字段并格式化输出
-            ;
+            var results = SelectAuthorDescending(books);
 
             // 输出结果
             foreach(var item in results)
             {
                 Console.WriteLine(item);
             }
+
+            // 统计指定作者的书籍数量
+            string authorToCount = "Sarah Connor";
+            int count = CountBooksByAuthor(books, authorToCount);
+            Console.WriteLine($"Number of books by {authorToCount}: {count}");
         }
     }
 
+    /// <summary>
+    /// 根据作者名称降序排序，并选择作者名称
+    /// </summary>
+    /// <param name="books"></param>
+    /// <returns></returns>
+    static IEnumerable<JToken?> SelectAuthorDescending(JArray books)
+    {
+        return books.OrderByDescending(b => b["author"]).Select(b => b["author"]);
+    }
+
+    /// <summary>
+    /// 根据作者名称过滤出指定作者的书籍数量
+    /// </summary>
+    /// <param name="books"></param>
+    /// <param name="author"></param>
+    /// <returns></returns>
+    static int CountBooksByAuthor(JArray books, string author)
+    {
+        return books.Count(b => b["author"]?.ToString() == author);
+    }
 }
