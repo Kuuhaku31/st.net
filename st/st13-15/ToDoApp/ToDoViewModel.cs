ad211fc512b723a92ba2f1f532c929f039de9610
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-03-18 10:27:29 +0800

    简单修改格式

--- st/st13-15/ToDoApp/ToDoViewModel.cs
+++ st/st13-15/ToDoApp/ToDoViewModel.cs
@@ -1,19 +1,27 @@
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
 using System.Collections.ObjectModel;
 
-namespace ToDoApp
+namespace ToDoApp;
+
+
+internal partial class ToDoViewModel : ObservableObject
 {
-    internal partial class ToDoViewModel : ObservableObject
+    private readonly ToDoModel _model;
+
+    public ObservableCollection<ToDo> ListViewRows { get; set; }
+
+    public ToDoViewModel()
     {
-        private ToDoModel _model;
+        _model = new();
+        ListViewRows = [];
 
-        public ObservableCollection<ToDo> ListViewRows { get; set; }
-
-        public ToDoViewModel()
+        // 添加数据到 ListViewRows
+        foreach (var todo in _model.ToDos)
         {
-            _model = new();
-            ListViewRows = new(_model.ToDos);
+            ListViewRows.Add(todo);
         }
     }
 }
